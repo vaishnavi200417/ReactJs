18da7068ca302faacd69f9cb05eef35586c525dc
vaishnavi <vaishudahiwal17@gmail.com>
2025-06-25 21:02:07 +0530

    EventClick

---
+++ EventClick.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function EventClick() {
+    function clickhandler() {
+        console.log("User clicked on button");
+    }
+    return (
+        <>
+            <div> EventClick</div>
+            <button onClick={clickhandler}>click Me</button>
+
+        </>
+
+
+    )
+}
+export default EventClick
