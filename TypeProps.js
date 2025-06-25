dd07b30c4ea57cae0c54e85e5418e287854cee2b
vaishnavi <vaishudahiwal17@gmail.com>
2025-06-25 20:45:33 +0530

    PropsType

---
+++ TypeProps.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types'
+
+class TypeProps extends Component {
+    render() {
+        const { myName, mynumber, value, anytype, bollean } = this.props
+        return (
+            < div >
+            <p>My name is {myName}</p>
+            <p>My age is {mynumber}</p>
+            <p>My value is {value}</p>
+            <p>My anytype {anytype}</p>
+            <p>bollean : {bollean ? 'true':'false'}</p>
+
+
+
+
+            </div>
+
+
+        )
+}
+}
+
+TypeProps.propTypes = {
+    myName: PropTypes.string.isRequired,
+    mynumber : PropTypes.number.isRequired,
+    value : PropTypes.array.isRequired,
+    anytype : PropTypes.any.isRequired,
+    boolean : PropTypes.bool.isRequired
+
+}
+export default TypeProps
