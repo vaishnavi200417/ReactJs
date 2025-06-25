5e082b06b43f7cb65705281cf659aecd43cfda22
vaishnavi <vaishudahiwal17@gmail.com>
2025-06-25 21:10:32 +0530

    ArgumentEvent

---
+++ ArgumentEvent.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function ArgumentEvent(){
+   const msg = (a)=>{
+        alert("Welcome" + a)
+    }
+    return(
+        <>
+        <div>ArgumentEvent</div>
+        <button onClick={()=>msg("MERN stack")}>Course</button>
+
+        </>
+
+    )
+}
+export default ArgumentEvent
+    
