3434fe20ffb6689fac26f7699ed5c1578c32abaf
vaishnavi <vaishudahiwal17@gmail.com>
2025-06-25 20:49:25 +0530

    Greet Components

---
+++ Greet.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Greet(props,{name,courseName}) {
+    console.log(props);
+    return(
+        <>
+        <div>Greet Components </div>
+        <div>Hello my name is :{name} My skills are {courseName}</div>
+        {props.children}
+        </>
+        
+
+    )
+    }
+
+
+export default Greet;
