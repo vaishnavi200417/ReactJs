03cabec8be6d64a679c2fab9b92369075d7330dc
vaishnavi <vaishudahiwal17@gmail.com>
2025-06-25 20:48:24 +0530

    state

---
+++ Message.js
@@ -0,0 +1,31 @@
+import React ,{component} from "react"
+
+class Message extends component {
+    constructor(){
+        super()
+        this.state={
+            msg : "Welcome to javascript"
+        }
+    }
+
+    changeMsg() {
+        this.setstate({
+            msg : "Welcome to ReactJs"
+        })
+
+    }
+
+
+    render() {
+        return (
+            <>
+            <div>{this.state.msg}</div>
+            <button onClick={()=>{this.changeMsg()}}>Change Msg</button>
+            </>
+           
+        )
+    }
+}
+
+    export default Message
+
