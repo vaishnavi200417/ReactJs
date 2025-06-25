e4b05b4742b5c80ee0cfacb249e847ca28290b49
vaishnavi <vaishudahiwal17@gmail.com>
2025-06-25 20:42:03 +0530

    Add files via upload

---
+++ Welcome.js
@@ -0,0 +1,17 @@
+import React ,{component} from "react"
+
+class Welcome extends component {
+    render(){
+        return(
+            <>
+            <div>Welcome components</div>
+            <h1>My Name is {this.props.name} and i am{this.props.skill}</h1>
+
+            </>
+
+        )
+    }
+}
+
+    export default Welcome
+
