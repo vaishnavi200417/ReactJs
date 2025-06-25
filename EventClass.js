fbf52e01af34a91a3688c614e1bc2404a4627e68
vaishnavi <vaishudahiwal17@gmail.com>
2025-06-25 21:03:02 +0530

    EventClass

---
+++ EventClass.js
@@ -0,0 +1,21 @@
+import React ,{component} from "react"
+
+class EventClass extends component {
+
+    clickhandler(){
+         console.log("User clicked on button");
+
+    }
+
+    render(){
+        return(
+            <>
+              <div> EventClick</div>
+            <button onClick={this.clickhandler}>click Me</button>
+            </>
+        )
+    }
+    }
+    export default EventClass
+
+    
