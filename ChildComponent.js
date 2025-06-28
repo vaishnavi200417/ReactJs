2727171d05f7662e697412f5c640557833c22940
vaishnavi <vaishudahiwal17@gmail.com>
2025-06-28 16:53:41 +0530

    ChildComponent

---
+++ ChildComponent.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ChildComponent (){
+    return(
+        <>
+        <div>ChildComponent</div>
+        <button onclick={props.parentHandler("ChildComponent")}>Click Me</button>
+        </>
+        
+
+    )
+    }
+
+
+export default ChildComponent
+
+
+
