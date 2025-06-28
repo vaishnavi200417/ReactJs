fd9b753759a3847a28ba450f98023c222e4d5952
vaishnavi <vaishudahiwal17@gmail.com>
2025-06-28 16:51:05 +0530

    person

---
+++ Persons.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Persons({person}) {
+    return (
+        <div><h3>My Name is {person.name}. I am {person.age} years old. My id is {person.id}
+        and my course is{person.course}</h3></div>
+
+    )
+}
+
+export default Persons
